901dd461c19b784cba52ab753a25e1aff714d1a5
Ubuntu <ubuntu@depot.asia-east1-a.c.mainsite-253206.internal>
2021-11-29 20:48:48 +0000

    temp

--- dataset.py
+++ dataset.py
@@ -140,9 +140,9 @@ class SpectrogramDataset(data.Dataset):
                     return np.array(images[0]), [MACHINE_CODE[emachine_code], temp]
         else:
             if len(images) == 1:
-                return np.array(images[0]), MACHINE_CODE[emachine_code]#np.array(images[0]), labels
+                return np.array(images[0]), labels#MACHINE_CODE[emachine_code]#np.array(images[0]), labels
             else:
-                return np.array(images[0]), MACHINE_CODE[emachine_code]#np.array(images), labels
+                return np.array(images[0]), labels#MACHINE_CODE[emachine_code]#np.array(images), labels
 
 
 def mono_to_color(X: np.ndarray,
